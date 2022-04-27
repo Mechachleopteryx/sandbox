64b0c74f375002ffbc9942a849725fcfab634fe4
standardgalactic <standardgalactic@protonmail.com>
2022-04-27 19:58:51 -0300

    Cargo build test.

---
+++ hello_cargo/target/debug/deps/hello_cargo-f07653fb14b5d1e3.d
@@ -0,0 +1,5 @@
+/home/phewf/projects/sandbox/hello_cargo/target/debug/deps/hello_cargo-f07653fb14b5d1e3: src/main.rs
+
+/home/phewf/projects/sandbox/hello_cargo/target/debug/deps/hello_cargo-f07653fb14b5d1e3.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/phewf/projects/sandbox/hello_cargo/target/debug/hello_cargo: /home/phewf/projects/sandbox/hello_cargo/src/main.rs
